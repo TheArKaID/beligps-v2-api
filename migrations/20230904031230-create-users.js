bf1f1e3b5560cf049a92fb9333c8339d37d98f26
Arifia Kasastra R <arka.progammer@gmail.com>
2023-09-05 08:52:20 +0700

    Creating user table and model

---
+++ migrations/20230904031230-create-users.js
@@ -0,0 +1,52 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING(50)
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING(255)
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING(25)
+      },
+      address: {
+        allowNull: false,
+        type: Sequelize.STRING(150)
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      created_by: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_by: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ models/User.js
@@ -0,0 +1,39 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {}
+  }
+  Users.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4
+    },
+    email: DataTypes.STRING(50),
+    password: DataTypes.STRING(255),
+    name: DataTypes.STRING(50),
+    phone: DataTypes.STRING(25),
+    address: DataTypes.STRING(150),
+    created_at: DataTypes.DATE,
+    created_by: DataTypes.STRING(50),
+    updated_at: DataTypes.DATE,
+    updated_by: DataTypes.STRING(50)
+  }, {
+    sequelize,
+    timestamps: true,
+    modelName: 'User',
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  });
+
+  return Users;
+};
